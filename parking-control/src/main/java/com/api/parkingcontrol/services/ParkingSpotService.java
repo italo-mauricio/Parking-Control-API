4dc2662caaeb5f4993499f2b038461779186942f
italo <italogithub@gmail.com>
2023-05-02 05:33:41 -0300

    creating save method

--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,6 +1,8 @@
 package com.api.parkingcontrol.services;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +13,9 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
