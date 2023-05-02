1309655944de61df23b49505ab107b5fc6ab3120
italo <italogithub@gmail.com>
2023-05-02 04:43:10 -0300

    update

---
+++ parking-control/src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,9 @@
+package com.api.parkingcontrol.repositories;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.rmi.server.UID;
+
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UID> {
+}
